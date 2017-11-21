f36135bcf86e538eb6d610ccb013a676f767c838
Miquel Adell Borràs <miquel@miqueladell.com>
2017-11-21 13:58:18 +0100

    not working

--- gulpfile.js
+++ gulpfile.js
@@ -15,15 +15,35 @@ var uglify = require('gulp-uglify');
 var clean = require('gulp-clean');
 var rename = require("gulp-rename");
 var runSequence = require('run-sequence');
-var gnf = require('gulp-npm-files');
-
 
 
 var config = {
-		bootstrapDir: './bower_components/bootstrap-sass',
-		distDir: './public',
-		srcDir: './source',
+	componentsDir: './node_modules',
+	distDir: './public',
+	srcDir: './source',
+	buildDir: './build',
+
+	browserSyncOptions: {
+		proxy: "http://limesurvey.dev/templates/limestrap/index.html",
+		notify: true
+	}
 };
+
+config.dependencies = {
+	scripts: [
+		config.componentsDir + '/jquery-ui-dist/jquery-ui.min.js',
+		config.componentsDir + '/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js'
+	],
+	styles: [
+	]
+};
+
+config.filesThatForceReload = [
+	config.srcDir + '/scripts/**/**',
+	config.srcDir + '/images/**/**',
+	'index.html'
+];
+
 
 function swallowError(self, error) {
 	console.log(error.toString());
@@ -32,97 +52,97 @@ function swallowError(self, error) {
 }
 
 gulp.task('styles', function() {
-	return runSequence('styles-generate', 'styles-minify');
-});
 
-gulp.task('styles-generate', function() {
-	return gulp.src(config.srcDir + '/styles/limestrap.scss')
-	.pipe(plumber({ errorHandler: function (error) { swallowError(this, error); } }))
+	return gulp.src([
+		config.srcDir + '/styles/limestrap.scss',
+	])
+	.pipe(sourcemaps.init())
 	.pipe(sass({
-		includePaths: [config.bootstrapDir + '/assets/stylesheets'],
+		includePaths: [
+			config.componentsDir + '/bootstrap/scss/',
+			config.componentsDir + '/awesome-bootstrap-checkbox/',
+			config.componentsDir + '/bootstrap-slider/src/sass/',
+			config.componentsDir + '/font-awesome/scss/'
+		]
 	}))
+	.pipe(sourcemaps.init())
+
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions'],
+		cascade: false
+	}))
+	.pipe(gulp.dest(config.distDir + '/styles'))
+	.pipe(cleanCSS({compatibility: 'ie8'}))
+
+	.pipe(concat('limestrap.css'))
+	.pipe(rename({suffix: '.min'}))
+	.pipe(sourcemaps.write('.'))
+
+	.pipe(browserSync.stream())
 	.pipe(gulp.dest(config.distDir + '/styles'));
 });
 
-gulp.task('styles-minify', function() {
-	return gulp.src(config.distDir + '/styles/*.css')
+gulp.task('scripts', function(){
+	return gulp.src([
+		config.srcDir + '/scripts/*.js',
+		config.buildDir + '/*.js'
+	])
 	.pipe(plumber({ errorHandler: function (error) { swallowError(this, error); } }))
-	.pipe(cleanCSS({compatibility: 'ie8'}))
-	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest(config.distDir + '/styles'))
-	.pipe(browserSync.stream());
-});
-
-gulp.task('scripts', function() {
-	return runSequence('scripts-generate', 'scripts-minify');
-});
-
-gulp.task('scripts-generate', function(){
-	return gulp.src(config.srcDir + '/scripts/**.js')
-	.pipe(plumber({ errorHandler: function (error) { swallowError(this, error); } }))
+	.pipe(sourcemaps.init())
 	.pipe(concat('limestrap.js'))
-	.pipe(gulp.dest(config.distDir + '/scripts/'));
-});
-
-gulp.task('scripts-minify', function() {
-	return gulp.src(config.distDir + '/scripts/*.js')
-	.pipe(plumber({ errorHandler: function (error) { swallowError(this, error); } }))
 	.pipe(stripDebug())
 	.pipe(uglify())
 	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest(config.distDir + '/scripts/'));
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest(config.distDir + '/scripts/'))
+	.pipe(browserSync.stream());
 });
 
 gulp.task('fonts', function() {
-	return gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
-	.pipe(plumber({ errorHandler: function (error) { swallowError(this, error); } }))
+	return gulp.src(
+		[
+			config.componentsDir + '/font-awesome/fonts/**/*'
+		]
+	)
 	.pipe(gulp.dest(config.distDir + '/fonts'));
 });
 
 gulp.task('images', function(){
 	return gulp.src(config.srcDir + '/images/**')
-	.pipe(plumber({ errorHandler: function (error) { swallowError(this, error); } }))
 	.pipe(imagemin())
 	.pipe(gulp.dest(config.distDir + '/images'));
 });
 
-gulp.task('copy-dependencies', function() {
-  gulp.src(gnf(), {base:'./'}).pipe(gulp.dest('./build'));
+gulp.task('copy-dependencies', ['clean'], function() {
+	gulp.src(config.dependencies.styles)
+	.pipe(gulp.dest(config.buildDir));
+	gulp.src(config.dependencies.scripts)
+	.pipe(gulp.dest(config.buildDir));
 });
 
 gulp.task('clean', function(){
-	return gulp.src(config.distDir, {read: false})
-	.pipe(plumber({ errorHandler: function (error) { swallowError(this, error); } }))
+	return gulp.src([config.distDir,config.buildDir], {read: false})
 	.pipe(clean());
 });
 
-gulp.task('clean-and-main', function(){
-	return runSequence('clean', 'main');
+gulp.task('default', function(){
+	return runSequence(
+		'clean-and-copy',
+		['styles', 'fonts', 'images', 'scripts']
+	);
 });
-
-gulp.task('main', function(){
-	return gulp.start('clean', 'styles', 'fonts', 'images', 'scripts');
-});
-gulp.task('default', ['clean-and-main']);
 
 gulp.task('watch', function () {
-
 	gulp.watch(config.srcDir + '/styles/**/**', ['styles']);
 	gulp.watch(config.srcDir + '/scripts/**/**', ['scripts']);
 	gulp.watch(config.srcDir + '/images/**/**', ['images']);
 
-	var filesThatForceReload = [
-		config.srcDir + '/scripts/**/**',
-		config.srcDir + '/images/**/**',
-		'index.html'
-	];
+	browserSync.init(config.browserSyncOptions);
+
+	gulp.watch(config.filesThatForceReload).on('change', browserSync.reload);
+});
 
 
-	var browserSyncOptions = {
-		proxy: "http://limesurvey.dev/templates/limestrap/index.html",
-		notify: true
-	};
-	browserSync.init(browserSyncOptions);
-
-	gulp.watch(filesThatForceReload).on('change', browserSync.reload);
+gulp.task('default', ['copy-dependencies'], function() {
+	gulp.start(['styles','fonts', 'images', 'scripts']);
 });
